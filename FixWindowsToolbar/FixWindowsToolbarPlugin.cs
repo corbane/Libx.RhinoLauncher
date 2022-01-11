0f64301a022a8a718bb162d97047dbb63752fa75
jmv <kitjmv@gmail.com>
2022-01-11 04:53:33 +0100

    Initial commit

---
+++ FixWindowsToolbar/FixWindowsToolbarPlugin.cs
@@ -0,0 +1,24 @@
+﻿
+using Rhino.PlugIns;
+
+
+namespace Libx.RhinoLauncher.FixWindowsToolbar
+{
+    public class FixWindowsToolbarPlugin : PlugIn
+    {
+        public static FixWindowsToolbarPlugin Instance { get; private set; }
+
+        public FixWindowsToolbarPlugin () { Instance = this; }
+        
+        public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+
+        protected override LoadReturnCode OnLoad (ref string errorMessage)
+        {
+            Microsoft.WindowsAPICodePack.Taskbar.TaskbarManager.Instance.SetApplicationIdForSpecificWindow (
+                Rhino.RhinoApp.MainWindowHandle (),
+                "Rhino " + Rhino.RhinoApp.ExeVersion.ToString ()
+            );
+            return base.OnLoad (ref errorMessage);
+        }
+    }
+}
